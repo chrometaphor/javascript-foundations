b9187d22b7a36c60419c52593e6c28612741d719
Chris Cole <christopher.a.cole@gmail.com>
2022-12-11 12:06:42 -0800

    Add unicorn test

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,28 @@
 class Unicorn {
   constructor(name, color) {
+    console.log(color)
     this.name = name;
-    this.color = color;
+    if (color === undefined) {
+      this.color = 'white';
+    } else {
+      this.color = color;
+    }
+  }
+  isWhite() {
+    console.log(this.color);
+    if (this.color !== 'white') {
+      return false;
+    }
+  }
+  says(message) {
+    return `**;* ${message} *;**`;
   }
 }
 
 module.exports = Unicorn;
+
+
+//read the test
+// ask if it will fail and why
+// run the test
+// make it pass
