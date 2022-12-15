3001cb333a4a7fee3dd1944d0f0293e4003fc995
Chris Cole <christopher.a.cole@gmail.com>
2022-12-14 21:55:41 -0800

    Add direwolf and stark to the direwolf-test, complete

---
+++ mythical-creatures/exercises/direwolf.js
@@ -0,0 +1,49 @@
+class Direwolf {
+ constructor (name, home, size) {
+    this.name = name;
+    this.home = home || "Beyond the Wall";
+    this.size = size || "Massive";
+    this.starksToProtect = []
+    this.huntsWhiteWalkers = true;
+ }
+protect (stark) {
+    if (this.starksToProtect.length > 1) {
+        return
+    }
+
+
+    if (this.home === stark.location) {
+        stark.safe = true 
+        this.starksToProtect.push(stark)
+        this.huntsWhiteWalkers = false;
+    }
+}
+    leave (stark) {
+        for (var i = 0; i < this.starksToProtect.length; i++) {
+            if (this.starksToProtect[i].name === stark.name) {
+                stark.safe = false;
+                this.starksToProtect.splice(i, 1)
+            }
+        }
+    }
+}
+
+module.exports = Direwolf;
+
+// BEGIN 1. it 'should be a function'
+// Classes. We have our class keyword. 
+// That's how we know it's a class.
+// It's capital D for direwolf, which is how
+// classes always begin.
+
+// class Direwolf {}
+
+// At the bottom we are exporting it with module.exports = Direwolf 
+// Every class needs a 
+
+// constructor() {}
+
+
+
+//2. 
+// 
